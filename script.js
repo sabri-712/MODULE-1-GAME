a8a1d8a323419ea497bc9ef394415c6628d1bbb0
Sabrina <sabrinamsmith1@gmail.com>
2023-07-14 07:46:50 -0400

    forgotten updates

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// quiz style game, pick your own adventure//
+/* quiz style game, pick your own adventure//
 // start game function
 // const startGame =()=>{
 //     console.log('Started')
@@ -8,28 +8,21 @@
 
 // startButton.addEventListener('click', startGame())
 
-
-// const startGame =()=>{
-//     console.log('Started')
-//     startButton.classList.add('hide')
-//     textboxContainerElement.classList.remove('hide')
-//     setNextQuestion()
-// }    
-
-// two options function
+// two options
 
 // If reader picks option A, go down path a function
-// const setNextQuestion=()=> {
-
-// }
 
 // IF reader picks option B, go down path b function
-
-// const intro = document.getElementsByClassName('introBox')
-
-
 
 // // PLOT: You are at a new job in a new city getting off late. 
 // You leave the office to go home when you realize someone is following you. 
 // Your mind races through a series of options as you try to decide what to do. 
-// Do you make it home?
+// Do you make it home?*/
+
+
+const optionA1 = ()=>{
+
+}
+const optionB1 = ()=>{
+    
+}
